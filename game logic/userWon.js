e2f4d68ba815541af7483435b7727f25843e30bf
יניר יניר <yanir401@gmail.com>
2022-05-03 16:42:01 +0300

    fixed new game button on popup and added timer win

--- game logic/userWon.js
+++ game logic/userWon.js
@@ -1,9 +1,14 @@
 const popUpElement = document.getElementById("pop-up");
 const overlay = document.getElementById("overlay");
 
-export function popUp(wrongGuessesCounter) {
-  console.log("sdsdsas");
+export function popUp(wrongGuessesCounter, timer) {
   popUpElement.classList.add("active");
   overlay.classList.add("active");
-  popUpElement.children[1].innerHTML = `Congratulations you won with ${wrongGuessesCounter} wrong guesses`;
+  popUpElement.children[1].innerHTML = `Congratulations you won with ${wrongGuessesCounter} wrong guesses in ${timer}`;
+}
+
+export function disablePopUp() {
+  popUpElement.classList.remove("active");
+  overlay.classList.remove("active");
+  popUpElement.children[1].innerHTML = "";
 }
