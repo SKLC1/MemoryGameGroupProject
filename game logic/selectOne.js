fb5e64a803600c71ad419c0d657396be2083094f
יניר יניר <yanir401@gmail.com>
2022-05-04 09:39:46 +0300

    added flip animation

--- game logic/selectOne.js
+++ game logic/selectOne.js
@@ -7,6 +7,8 @@ export function addEventToGrid(grid, selectedOne, selectedTwo, updateCounter) {
       if (e.target.className.includes("card")) {
         e.target.classList.remove("unselected");
       }
+      e.target.classList.add("flip");
+      e.target.classList.remove("backFlip");
       if (!selectedOne || selectedOne === e.target) {
         // if user press the same card twice selectedTwo wont update
         selectedOne = e.target;
@@ -19,6 +21,7 @@ export function addEventToGrid(grid, selectedOne, selectedTwo, updateCounter) {
           selectedTwo.classList.remove("unselected");
           selectedOne.classList.add("flipped");
           selectedTwo.classList.add("flipped");
+
           selectedOne = null;
           selectedTwo = null;
           updateCounter(true);
@@ -27,6 +30,10 @@ export function addEventToGrid(grid, selectedOne, selectedTwo, updateCounter) {
           setTimeout(() => {
             selectedOne.classList.add("unselected");
             selectedTwo.classList.add("unselected");
+            selectedOne.classList.remove("flip");
+            selectedTwo.classList.remove("flip");
+            selectedOne.classList.add("backFlip");
+            selectedTwo.classList.add("backFlip");
             selectedOne = null;
             selectedTwo = null;
           }, 1000);
